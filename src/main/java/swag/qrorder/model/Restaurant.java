126e5cee0c1b2445b4e418d20a90b15cc6efa5a6
JaeUng <wodndrnjs@naver.com>
2023-10-09 11:18:20 +0900

    Restaurant 관련 로직 추가(3) - qrkey 로 메뉴 검색 result map 변경

--- src/main/java/swag/qrorder/model/Restaurant.java
+++ src/main/java/swag/qrorder/model/Restaurant.java
@@ -1,10 +1,12 @@
 package swag.qrorder.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import javax.validation.constraints.Pattern;
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 public class Restaurant {
     @Pattern(regexp = "^(\\d{3})-(\\d{2})-(\\d{5})$", message = "사업자 번호 형식이 맞지않습니다.")
